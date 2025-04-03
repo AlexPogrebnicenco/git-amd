b0c3b92fa3f38c1e7785462a4a2d6013453ee5a4
Alexandru Pogrebnicenco <alexpogrebnicenco@gmail.com>
2025-04-03 20:53:07 +0300

    text was changed by the first editor

--- testFirst/testFirst/Program.cs
+++ testFirst/testFirst/Program.cs
@@ -10,7 +10,7 @@ namespace testFirst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello 123");
+            Console.WriteLine("Hello from first editor");
             Console.ReadKey();
         }
     }
